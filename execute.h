036c2eaf4328cb9934c1453116d5b39efb3aa0a8
sherows <sherowsorganics@gmail.com>
2023-09-19 18:08:51 +0300

    update

--- execute.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXECUTE_H
-#define EXECUTE_H
-
-#include <shell.h>
-
-void execute_command(char *command);
-
-#endif /* EXECUTE_H */
--- prompt.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROMPT_H
-#define PROMPT_H
-#include "shell.h"
-
-#define PROMPT "#cisfun$ "
-
-void display_prompt(void);
-
-#endif /* PROMPT_H */
